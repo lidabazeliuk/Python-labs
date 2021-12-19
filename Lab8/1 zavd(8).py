e0906959a63fe48bb2de35a7cd69de95bbce7a7c
lidabazeliuk <93678798+lidabazeliuk@users.noreply.github.com>
2021-12-19 17:29:09 +0200

    Update 1 zavd(8).py

--- Lab8/1 zavd(8).py
+++ Lab8/1 zavd(8).py
@@ -1,10 +1,10 @@
 import math
 def f(x,y):
     if x > y:
-        return x ** 3 + math.fabs(x ** 2 + x ** 4)
+        return x ** 3 + math.sqrt(x ** 2 + x ** 4)
 
     else:
-        return (x ** 2 - 2 * x + math.fabs(x)) / x ** (3 / 5)
+        return (x ** 2 - 2 * x + math.sqrt(x)) / x ** (3 / 5)
 a = float(input('a = '))
 b = float(input('b = '))
 U = f(a,b) + f(2,a) + 2
